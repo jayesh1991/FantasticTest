7f69a58468999b08469700aa0489aa289861418d
jayesh rajput <jayeshrajput30@gmail.com>
2018-03-07 13:13:58 +0530

    test
    first commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Imma Web Pvt Ltd on 07/03/18.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+
+}
